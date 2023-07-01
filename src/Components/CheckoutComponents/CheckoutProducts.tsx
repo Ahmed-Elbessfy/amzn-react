bcc28d4dbecfda5e2349fdc4455b3c192c6f16e7
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2023-07-01 13:53:22 +0300

    get checkout products at CheckoutProducts comp
    - Consume CheckoutProducts state to get checkoutProducts
    - Display product title as a trail to check consuming success

--- src/Components/CheckoutComponents/CheckoutProducts.tsx
+++ src/Components/CheckoutComponents/CheckoutProducts.tsx
@@ -1,11 +1,27 @@
 import { FC } from "react";
 
+import { RootState } from "store";
+import { useSelector } from "react-redux";
+
 const CheckoutProducts: FC = () => {
+  // get selected checkout products
+  const selectedCheckoutProductsList = useSelector(
+    (state: RootState) => state.checkoutProductsReducer.checkoutProductsList
+  );
+
   return (
     <div className="checkout_products_container">
       <h1 className="checkout_title">Your shopping basket</h1>
 
-      <div className="checkout_products"></div>
+      <div className="checkout_products">
+        {selectedCheckoutProductsList.map((product) => {
+          return (
+            <>
+              <p key={product.id}> {product.title}</p> <br />
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 };
