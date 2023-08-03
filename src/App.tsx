089b7f0f542c9ccb667a8f4ef9ef4a6a9694f6cf
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2023-08-03 17:47:54 +0300

    add and use sign_up route & SignUpPage view
    - create signPage file to view sign up page content
    - import SignUpPage in App to create route
    - create route for Sign up view displayed by SignUpPage page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import CheckoutPage from "./Pages/CheckoutPage";
 import LoginPage from "./Pages/LoginPage";
+import SignUpPage from "./Pages/SignUpPage";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/checkout" element={<CheckoutPage />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/sign_up" element={<SignUpPage />} />
           </Routes>
         </Router>
       </Provider>
